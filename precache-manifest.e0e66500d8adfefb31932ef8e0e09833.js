c69a97c22e2504b661104e8671ebe9cd8fda6191
Christina Kim <christinahyunakim@gmail.com>
2019-03-10 17:04:08 -0500

    Updates

---
+++ precache-manifest.e0e66500d8adfefb31932ef8e0e09833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ed672ce5482582fe83f3c6e9c46b4c3",
+    "url": "/workflow/static/media/feather-sprite.0ed672ce.svg"
+  },
+  {
+    "revision": "a9071609711347d3299b",
+    "url": "/workflow/static/js/runtime~main.a9071609.js"
+  },
+  {
+    "revision": "04439b0467c733d1abc6",
+    "url": "/workflow/static/js/main.04439b04.chunk.js"
+  },
+  {
+    "revision": "477189ec1333a97932cb",
+    "url": "/workflow/static/js/2.477189ec.chunk.js"
+  },
+  {
+    "revision": "04439b0467c733d1abc6",
+    "url": "/workflow/static/css/main.1e90b3df.chunk.css"
+  },
+  {
+    "revision": "477189ec1333a97932cb",
+    "url": "/workflow/static/css/2.e64e4d6c.chunk.css"
+  },
+  {
+    "revision": "de90c905c34f323ec524d80b39a0d251",
+    "url": "/workflow/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/workflow/precache-manifest.52c4384b456f76dd887595f2b83d49ee.js"
+  "/workflow/precache-manifest.e0e66500d8adfefb31932ef8e0e09833.js"
 );
 
 workbox.clientsClaim();
