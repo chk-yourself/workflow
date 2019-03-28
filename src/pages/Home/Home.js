3f33f18c0b243a2a13b726a592b8c382ecdc1545
Christina Kim <christinahyunakim@gmail.com>
2019-03-28 07:51:44 -0500

    fixed sidebar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -87,7 +87,7 @@ class HomePage extends Component {
           <Route
             path={ROUTES.USER_PROJECTS}
             render={props => (
-              <Main title="Projects" classes={{title: "project-grid__header"}}>
+              <Main title="Projects" classes={{main: 'project-grid__container', title: "project-grid__header"}}>
                 <ProjectGridContainer
                   userId={userId}
                   openProjectComposer={this.toggleProjectComposer}
