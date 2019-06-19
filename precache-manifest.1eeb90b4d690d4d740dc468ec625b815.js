9e7178968d2aab145ec8978a9d3b3cf99239dcad
Christina Kim <christinahyunakim@gmail.com>
2019-06-19 02:04:28 -0500

    Updates

---
+++ precache-manifest.1eeb90b4d690d4d740dc468ec625b815.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ff2fb5a594bd8c7450faa0caffab906",
+    "url": "/static/media/feather-sprite.0ff2fb5a.svg"
+  },
+  {
+    "revision": "2c8f7d4b4e91c1de7bd7",
+    "url": "/static/css/main.2201e271.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f93f6a4586c422018f39a1b940941e60",
+    "url": "/static/media/scrum-board.f93f6a45.svg"
+  },
+  {
+    "revision": "6a5f1858986f98bb459e",
+    "url": "/static/js/2.b7d8bc34.chunk.js"
+  },
+  {
+    "revision": "2c8f7d4b4e91c1de7bd7",
+    "url": "/static/js/main.c937a78e.chunk.js"
+  },
+  {
+    "revision": "15d6dad8d5c7dfa5391d710c2f90b617",
+    "url": "/static/media/jam-sprite.15d6dad8.svg"
+  },
+  {
+    "revision": "3703df9c67674402de263b0b987d5989",
+    "url": "/static/media/todo-list.3703df9c.svg"
+  },
+  {
+    "revision": "0d8ad8d8d7c84a9a402d9359dd4798e0",
+    "url": "/static/media/work-chat.0d8ad8d8.svg"
+  },
+  {
+    "revision": "6a5f1858986f98bb459e",
+    "url": "/static/css/2.e64e4d6c.chunk.css"
+  },
+  {
+    "revision": "7994a5ee3ae6b69a13379efee978130e",
+    "url": "/index.html"
+  }
+];
